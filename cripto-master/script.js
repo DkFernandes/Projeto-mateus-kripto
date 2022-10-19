257168e330728679baf409929cc7ceff1de665bb
Deka Fernandes <111399129+DkFernandes@users.noreply.github.com>
2022-10-19 19:55:32 -0300

    commit

---
+++ cripto-master/script.js
@@ -0,0 +1,8 @@
+function botao(){
+    var caixote = document.getElementById("magica")
+    if (caixote.style.display =="block"){
+        caixote.style.display ="none"
+    } else {
+        caixote.style.display = "block"
+    }
+}
